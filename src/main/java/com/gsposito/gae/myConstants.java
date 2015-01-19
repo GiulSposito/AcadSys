e0c1cfaa77336a614c99bccc67d09bad85c4cf66
unknown <gsposito@CIT007408.cit>
2015-01-19 11:20:00 -0200

    Criacao de myConstant.java e remoção do Constant do index para que os valores de chaves não subam para o GitHub

---
+++ src/main/java/com/gsposito/gae/myConstants.java
@@ -0,0 +1,15 @@
+package com.gsposito.gae;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming your API.
+ * 
+ * This is a prototype, you must fill the correct values below and rename de class to Constants
+ */
+public class myConstants {
+  public static final String WEB_CLIENT_ID = "replace this with your Android client ID";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
